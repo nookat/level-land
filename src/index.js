248f4268a241a3a84ddfeb7f7a6f8d7debabd584
Eugene-prog <ieugene.prog@gmail.com>
2023-04-02 22:51:32 +0700

    feat: add footer up part in layout

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import Footer from "./components/Footer";
+import "./css/style.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
     <Footer />
   </React.StrictMode>
 );
