3bccd6e45e5338232c92090616d79cc012a7f6ee
Alexander Sosov <nookat.develop@gmail.com>
2023-04-04 09:54:19 +0300

    feat: подключить стили

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-function App() {
-  return (
-    <div className="App">
-      <p>hello world</p>
-    </div>
-  );
-}
+import React from 'react';
+import 'normalize.css';
+import './styles/main.scss';
+
+const App = () => (
+  <div className="App">
+    <p>hello world</p>
+  </div>
+);
 
 export default App;
