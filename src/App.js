f5d6fd1599f565b7c11fba1c69426c25240bce19
Alexander Sosov <nookat.develop@gmail.com>
2023-04-04 10:00:34 +0300

    feat: подключить footer и header

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import React from 'react';
 import 'normalize.css';
 import './styles/main.scss';
 
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+
 const App = () => (
   <div className="App">
-    <p>hello world</p>
+    <Header/>
+    <Footer/>
   </div>
 );
 
