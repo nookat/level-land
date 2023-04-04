916d53b37f4e02ed21a369c6cf34b5a23b1487e2
Alexander Sosov <nookat.develop@gmail.com>
2023-04-04 09:57:16 +0300

    feat: создать footer и header

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import Footer from "./components/Footer";
-import "./css/style.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Footer />
+    <App/>
   </React.StrictMode>
 );
